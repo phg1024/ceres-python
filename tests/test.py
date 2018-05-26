6ebcef3d88d5d9a8503989d1b45cdbf5c1001cb2
Peihong Guo <peihongguo@gmail.com>
2018-05-26 01:35:07 -0700

    updated test

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,15 @@
-from ceres import optimize
+from ceres_python import optimize
+from functools import partial
 import numpy as np
 
-def func(ps):
-    return np.sum(ps**2)
+def func(ps, target=None):
+    assert(ps.shape == target.shape)
+    return np.sum((ps-target)**2)
 
-def grad(ps):
-    return 2 * ps
+def grad(ps, target=None):
+    return 2 * (ps - target)
 
-x0 = np.array([1,2,3,4,5,6,7,8,9,10], dtype=np.double)
+x0 = np.array(range(1, 6), dtype=np.float)
+target = np.array([3.725] * 5)
 
-print(optimize(func, grad, x0))
+print(optimize(partial(func, target=target), partial(grad, target=target), x0))
